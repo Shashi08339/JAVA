edacd94327ee844f1a769d7f9f0d7e240f93c162
Shashi08339 <shashitejakasetty@gmail.com>
2025-08-21 16:50:34 +0530

    array

---
+++ array.java
@@ -0,0 +1,16 @@
+class array{
+    public static void main (String[] args){
+        int n[] =new int[6];
+        n[0]=1;
+        n[1]=1;
+        n[2]=1;
+        n[3]=1;
+        n[4]=1;
+        n[5]=1;
+        System.err.println(n[3]);
+    }
+}
+
+// home work 21-08-25
+// take a number and check its even or odd 
+// take a number and print it's square
