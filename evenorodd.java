bcaae666bc2211b62ed0bf904e04cfe608a77c7c
Shashi08339 <shashitejakasetty@gmail.com>
2025-08-21 17:28:10 +0530

    even or odd number

---
+++ evenorodd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class evenorodd {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("enter a number");
+        int a=sc.nextInt();
+        if(a % 2== 0){
+            System.out.println("brohh I'm even number");
+        }
+        else{
+            System.out.println("beohh I'm odd number");
+        }
+        sc.close();
+    }
+    
+}
+
+
+
