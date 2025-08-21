fe14d931a32bd200ca972085a1438e99973150c6
Shashi08339 <shashitejakasetty@gmail.com>
2025-08-21 16:50:08 +0530

    greater

---
+++ greater.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class greater{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a>b){
+            System.out.println("a is  big");
+        }
+        if (a<b) {
+            System.out.println("a is small");
+            
+        } 
+        else { 
+            System.out.println("a=b");
+        }
+
+
+    }
+
+}
